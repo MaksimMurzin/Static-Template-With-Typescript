da096af962f951fa86a5bf7b67af457f2cd41482
MaksimMurzin <maks_murzin@yahoo.com>
2025-10-13 00:01:09 +0100

    ready for github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
+
 export default defineConfig({
   root: ".",
-  build: { outDir: "dist" },
+  base: "/Static-Template-With-Typescript/",
+  build: {
+    outDir: "dist",
+  },
 });
